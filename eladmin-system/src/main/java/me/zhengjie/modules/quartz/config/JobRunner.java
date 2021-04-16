67f6e769e8c54d6826a2e194c91c3c6485ac6b9a
yangjianfei <yangjianfeifly@163.com>
2021-04-16 17:46:29 +0800

    资源配置状态初始化

--- eladmin-system/src/main/java/me/zhengjie/modules/quartz/config/JobRunner.java
+++ eladmin-system/src/main/java/me/zhengjie/modules/quartz/config/JobRunner.java
@@ -15,10 +15,13 @@
  */
 package me.zhengjie.modules.quartz.config;
 
+import cn.hutool.core.util.ObjectUtil;
 import lombok.RequiredArgsConstructor;
 import me.zhengjie.modules.quartz.domain.QuartzJob;
 import me.zhengjie.modules.quartz.repository.QuartzJobRepository;
 import me.zhengjie.modules.quartz.utils.QuartzManage;
+import me.zhengjie.modules.tool.domain.ResourcesManagement;
+import me.zhengjie.modules.tool.service.ResourcesManagementService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.ApplicationArguments;
@@ -37,6 +40,7 @@ public class JobRunner implements ApplicationRunner {
     private static final Logger log = LoggerFactory.getLogger(JobRunner.class);
     private final QuartzJobRepository quartzJobRepository;
     private final QuartzManage quartzManage;
+    private final ResourcesManagementService resourcesManagementService;
 
     /**
      * 项目启动时重新激活启用的定时任务
@@ -49,5 +53,17 @@ public class JobRunner implements ApplicationRunner {
         List<QuartzJob> quartzJobs = quartzJobRepository.findByIsPauseIsFalse();
         quartzJobs.forEach(quartzManage::addJob);
         log.info("--------------------定时任务注入完成---------------------");
+
+        // 检查是否有状态为启动的配置
+        ResourcesManagement resourcesManagement = resourcesManagementService.findByEnabled();
+        /*  此处有两个选择
+                   1.根据已启动状态的配置注入Bean (需要调整顺序 此时执行找不到BeanFactory
+                   2.修改数据库内数据状态为未启动 (此处使用的这个
+         */
+        if (ObjectUtil.isNotNull(resourcesManagement)) {
+            // 禁用 启用状态的资源配置
+            resourcesManagementService.updateById(resourcesManagement.getId());
+        }
+        log.info("--------------------资源配置初始化完成---------------------");
     }
 }
