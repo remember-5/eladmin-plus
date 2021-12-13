a385cbf80bf521545b032d1c64fc5057010219f8
wangjiahao <1332661444@qq.com>
2021-12-13 14:57:53 +0800

    测试全部引号

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 nohup java -jar eladmin-template.jar \
---DB_HOST=118.25.95.207 \
---DB_PORT=3306 \
---DB_NAME=eladmin_template \
---DB_USER=eladmin_template \
---DB_PWD=EladminTemplate@2021 \
---REDIS_HOST=118.25.95.207 \
---REDIS_PORT=6379 \
---REDIS_PWD=lQrVBDbu9rR7T3oY \
---REDIS_DB=9 \
---DRUID_USER=admin \
---DRUID_PWD=QrVBDbu9rR7T3o > nohup.out 2>&1 &
+--DB_HOST= \
+--DB_PORT= \
+--DB_NAME= \
+--DB_USER= \
+--DB_PWD= \
+--REDIS_HOST= \
+--REDIS_PORT= \
+--REDIS_PWD= \
+--REDIS_DB= \
+--DRUID_USER= \
+--DRUID_PWD= > nohup.out 2>&1 &
