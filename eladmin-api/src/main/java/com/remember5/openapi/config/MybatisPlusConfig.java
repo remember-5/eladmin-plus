4da7b9f7bc5a10c52fe46192cd90aedacc3d2b54
wangjiahao <1332661444@qq.com>
2023-12-26 16:58:54 +0800

    完善三方依赖包

--- eladmin-api/src/main/java/com/remember5/openapi/config/MybatisPlusConfig.java
+++ eladmin-api/src/main/java/com/remember5/openapi/config/MybatisPlusConfig.java
@@ -1,6 +1,5 @@
 package com.remember5.openapi.config;
 
-import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.springframework.context.annotation.Bean;
@@ -18,8 +17,7 @@ public class MybatisPlusConfig {
     @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
-        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
-        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.POSTGRE_SQL));
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor());
         return interceptor;
     }
 
