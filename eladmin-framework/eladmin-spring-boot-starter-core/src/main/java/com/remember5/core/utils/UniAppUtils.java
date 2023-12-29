54f834966711227160aa61b8ee4c75df688444b8
wangjiahao <1332661444@qq.com>
2023-12-29 13:56:48 +0800

    完善spring最新自动装配

--- eladmin-framework/eladmin-spring-boot-starter-core/src/main/java/com/remember5/core/utils/UniAppUtils.java
+++ eladmin-framework/eladmin-spring-boot-starter-core/src/main/java/com/remember5/core/utils/UniAppUtils.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright [2022] [remember5]
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.remember5.core.utils;
 
 import com.alibaba.fastjson2.JSON;
@@ -47,7 +62,7 @@ public class UniAppUtils {
                 appInformation.setAppid(manifestJson.getString("id"));
                 appInformation.setName(manifestJson.getString("name"));
                 appInformation.setDescription(manifestJson.getString("description"));
-                
+
                 appInformation.setVersion(versionJson.getString("name"));
                 appInformation.setBuild(versionJson.getString("code"));
                 return appInformation;
